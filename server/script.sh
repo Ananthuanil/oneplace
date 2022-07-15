a10fbe280d3332bf5adc83266f111735b1c2b7bd
amaljith-tr <108731315+amaljith-tr@users.noreply.github.com>
2022-07-15 14:48:18 +0530

    Update script.sh

--- server/script.sh
+++ server/script.sh
@@ -2,6 +2,6 @@
 #Add for intializing
 #npm run db:init
 #npx prisma migrate deploy
-#npx prisma migrate dev --preview-feature
-DATABASE_URL="postgresql://postgres:admin@localhost/oneplace?host=/cloudsql/cloud-run-dev-348705:us-central1:oneplace-db" npx prisma migrate deploy --preview-feature
+npx prisma migrate dev --preview-feature
+npx prisma migrate deploy --preview-feature
 node dist/main
