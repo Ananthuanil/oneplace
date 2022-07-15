4d46a070f9052cee5dad0e3c7de246c313c3f389
amaljith-tr <amaljith.tr@factweavers.com>
2022-07-15 18:12:59 +0530

    update env

--- server/script.sh
+++ server/script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-DATABASE_URL="postgresql://postgres:admin@localhost/oneplace?host=/cloudsql/cloud-run-dev-348705:us-central1:oneplace-db" npx prisma migrate deploy --preview-feature
+npx prisma migrate deploy --preview-feature
 node dist/main
