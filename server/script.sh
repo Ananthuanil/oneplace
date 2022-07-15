1027d630e9ed7d19a5bb88b6caca37e275feb1ae
amaljith-tr <amaljith.tr@factweavers.com>
2022-07-15 18:41:41 +0530

    script fix

--- server/script.sh
+++ server/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-npx prisma migrate resolve --applied "20220715121425_init" --preview-feature
+#npx prisma migrate resolve --applied "20220715121425_init" --preview-feature
 npx prisma migrate deploy --preview-feature
 node dist/main
