306445e7ea34aaf8deae51f7b6897ffa9870c2f4
amaljith-tr <amaljith.tr@factweavers.com>
2022-08-09 10:14:03 +0530

    updated script

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -28,7 +28,7 @@ async function seed(bcryptSalt: Salt) {
   const data = {
     username: "admin",
     password: await hash("admin", bcryptSalt),
-    roles: ["user"],
+    roles: ["superAdmin"],
     designation: [],
     firstName: "",
     lastName: "",
