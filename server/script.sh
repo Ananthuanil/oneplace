b23204feb2363a4c15d8833937d5b2a2c432ca5a
amaljith-tr <amaljith.tr@factweavers.com>
2022-08-17 12:20:55 +0530

    fix

--- server/script.sh
+++ server/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#npx prisma migrate resolve --applied "20220715121425_init" --preview-feature
-npx prisma migrate deploy --preview-feature
+npx prisma migrate resolve --applied "20220817064659_baseline_migration" --preview-feature
+# npx prisma migrate deploy --preview-feature
 # npm run db:clean
 # npm run db:init
 node dist/main
