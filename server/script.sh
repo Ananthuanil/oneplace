ad3fa928c6cc634b9fb935970e761f7ebf42c5a7
amaljith-tr <108731315+amaljith-tr@users.noreply.github.com>
2022-08-17 13:00:20 +0530

    Update script.sh

--- server/script.sh
+++ server/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # npx prisma migrate resolve --applied "20220817064659_baseline_migration" --preview-feature
-npx prisma migrate dev --create-only
+# npx prisma migrate dev --create-only
 npx prisma migrate deploy --preview-feature
 # npm run db:clean
 # npm run db:init
