64ecbb7e6d88d1bd156c4cdf7d74f1b0b396cb96
amaljith-tr <amaljith.tr@factweavers.com>
2022-09-23 12:41:16 +0530

    script update

--- server/script.sh
+++ server/script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # npx prisma migrate resolve --applied "20220817064659_baseline_migration" --preview-feature
 # npx prisma migrate dev --create-only
-# npx prisma migrate deploy --preview-feature
-npx prisma migrate resolve --applied "20220923061625_updated_interview_added_email_status"
+npx prisma migrate deploy --preview-feature
+# npx prisma migrate resolve --applied "20220923061625_updated_interview_added_email_status"
 # npm run db:clean
 # npm run db:init
 node dist/main
